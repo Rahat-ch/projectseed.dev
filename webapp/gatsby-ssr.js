3350d968a25787cfbec0dd66fcb97c80df9e8834
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2020-02-12 00:18:07 -0500

    added authentication

--- webapp/gatsby-ssr.js
+++ webapp/gatsby-ssr.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from 'react';
+import { navigate } from 'gatsby';
+import { AuthProvider } from 'react-use-auth';
 
-// You can delete this file if you're not using it
+export const wrapRootElement = ({ element }) => (
+  <AuthProvider navigate={navigate} auth0_domain={process.env.GATSBY_AUTH0DOMAIN} auth0_client_id={process.env.GATSBY_AUTH0CLIENTID}>
+    {element}
+  </AuthProvider>
+);
--- webapp/src/pages/index.js
+++ webapp/src/pages/index.js
@@ -3,13 +3,21 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { useAuth } from 'react-use-auth';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Welcome" />
-    <h1>Welcome to Project Seed Dev</h1>
-    <Link to="/home/">Go to home</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const { isAuthenticated, user, login } = useAuth()
+  console.log(user)
+  return (
+    <Layout>
+      <SEO title="Welcome" />
+      <h1>Welcome to Project Seed Dev</h1>
+      <button onClick={login}>Log in/Sign Up</button>
+      {isAuthenticated() ? <p>Hello {user.name}</p> : null}
+      {isAuthenticated() ? <img src={user.picture} alt="userprofileimage" /> : null}
+      <Link to="/home/">Go to home</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage

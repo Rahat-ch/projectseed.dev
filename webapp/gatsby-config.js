db16e1790e46937cac6bfa488dab575571698851
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2020-02-14 00:29:04 -0500

    serverless configured and webapp connected

--- webapp/gatsby-config.js
+++ webapp/gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   siteMetadata: {
     title: `Project Seed Dev`,
@@ -25,6 +29,17 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // Arbitrary name for the remote schema Query type
+        typeName: "PSDAPI",
+        // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
+        fieldName: "psdapi",
+        // Url to query from
+        url: process.env.GATSBY_APIENDPOINT,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
